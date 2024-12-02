2eb6a6c7e7656f510af9af5754a47f8918588896
Elias <150734575+Sir-Eddy@users.noreply.github.com>
2024-12-02 13:21:22 +0100

    Completed Day 1

---
+++ Day 1 - Historian Hysteria/Part1.py
@@ -0,0 +1,28 @@
+def readfile():
+    with open('list.txt', 'r') as f:
+        return f.readlines()
+
+def __main__():
+    lines = readfile()
+    total = 0
+    array_a = []
+    array_b = []
+    for line in lines:
+        temp_array = list(map(int, line.split()))
+        a = temp_array[0]
+        array_a.append(a)
+        b = temp_array[1]
+        array_b.append(b)
+    array_a.sort()
+    array_b.sort()
+    for i in range(len(array_a)):
+        if array_a[i] < array_b[i]:
+            total += array_b[i] - array_a[i]
+        else:
+            total += array_a[i] - array_b[i]
+    print(total)
+    
+
+
+if __name__ == "__main__":
+    __main__()
---
+++ Day 1 - Historian Hysteria/Part2.py
@@ -0,0 +1,27 @@
+def readfile():
+    with open('list.txt', 'r') as f:
+        return f.readlines()
+
+def __main__():
+    lines = readfile()
+    total = 0
+    array_a = []
+    array_b = []
+    for line in lines:
+        temp_array = list(map(int, line.split()))
+        a = temp_array[0]
+        array_a.append(a)
+        b = temp_array[1]
+        array_b.append(b)
+    for i in range(len(array_a)):
+        total_tmp = 0
+        for j in range(len(array_b)):
+            if array_a[i] == array_b[j]:
+                total_tmp += 1
+        total += total_tmp * array_a[i]
+    print(total)
+    
+
+
+if __name__ == "__main__":
+    __main__()
